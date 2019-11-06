3e51e23c9a22444071b820fb48bdc6e48fefa52e
edermarcelo10 <edermarcelo10@gmail.com>
2019-11-06 10:33:40 -0300

    Create Artista.java
    Criação de classe Artista e seus atributos.

---
+++ ProjAlbum/src/main/java/ProjAlbuns/Artista.java
@@ -0,0 +1,19 @@
+package ProjAlbuns;
+
+public class Artista {
+	public Integer Id_Artista;
+  	public String Nome_Artista;
+  
+  	public Integer getId_Artista() {
+		return Id_Artista;
+	}
+	public void setId_Artista(Integer Id_Artista) {
+		this.Id_Artista = Id_Artista;
+	}
+	public String getNome_Artista() {
+		return Nome_Artista;
+	}
+	public void setNome_Artista(String Nome_Artista) {
+		this.Nome_Artista = Nome_Artista;
+	}
+}
