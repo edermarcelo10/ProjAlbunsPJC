1c0d13878c17a4b2f96063be62a7562aea415545
edermarcelo10 <edermarcelo10@gmail.com>
2019-11-06 12:42:12 -0300

    Create ProjAlbunsPjcApplication.java
    Recriando projeto e configurando.
    Incluindo as classes e configuração de banco de dados Postgresql.

---
+++ ProjAlbunsPJC/src/main/java/com/ProjAlbunsPJC/api/ProjAlbunsPjcApplication.java
@@ -0,0 +1,13 @@
+package com.ProjAlbunsPJC.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjAlbunsPjcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjAlbunsPjcApplication.class, args);
+	}
+
+}
