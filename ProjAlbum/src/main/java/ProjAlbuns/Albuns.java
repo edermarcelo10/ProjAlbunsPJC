f65d871ff8569d0e82150845d3a387bb0d8b7756
edermarcelo10 <edermarcelo10@gmail.com>
2019-11-06 10:33:13 -0300

    Create Albuns.java
    Criação de classe Albuns e seus atributos.

---
+++ ProjAlbum/src/main/java/ProjAlbuns/Albuns.java
@@ -0,0 +1,26 @@
+package ProjAlbuns;
+
+public class Albuns {
+	public Integer Id_Album;
+  	public String Descricao_Album;
+  	public Integer Id_Artista;
+  	
+  	public Integer getId_Album() {
+		return Id_Album;
+	}
+	public void setId_Album(Integer Id_Album) {
+		this.Id_Album = Id_Album;
+	}
+	public String getDescricao_Album() {
+		return Descricao_Album;
+	}
+	public void setDescricao_Album(String Descricao_Album) {
+		this.Descricao_Album = Descricao_Album;
+	}
+	public Integer getId_Artista() {
+		return Id_Artista;
+	}
+	public void setId_Artista(Integer Id_Artista) {
+		this.Id_Artista = Id_Artista;
+	}
+}
