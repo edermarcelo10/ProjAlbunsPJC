c149e85ce2ebec494ea973cece98c8f2fdf36363
edermarcelo10 <edermarcelo10@gmail.com>
2019-11-06 12:42:02 -0300

    Create Album.java
    Recriando projeto e configurando.
    Incluindo as classes e configuração de banco de dados Postgresql.

---
+++ ProjAlbunsPJC/src/main/java/com/models/Album.java
@@ -0,0 +1,46 @@
+package com.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.sun.istack.NotNull;
+
+public class Album {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long Id_Album;
+	
+	@NotNull
+	private String Nome_Album;
+	
+	@NotNull
+	private long Id_Artista;
+
+	public long getId_Album() {
+		return Id_Album;
+	}
+
+	public void setId_Album(long id_Album) {
+		Id_Album = id_Album;
+	}
+
+	public String getNome_Album() {
+		return Nome_Album;
+	}
+
+	public void setNome_Album(String nome_Album) {
+		Nome_Album = nome_Album;
+	}
+
+	public long getId_Artista() {
+		return Id_Artista;
+	}
+
+	public void setId_Artista(long id_Artista) {
+		Id_Artista = id_Artista;
+	}
+	
+}
