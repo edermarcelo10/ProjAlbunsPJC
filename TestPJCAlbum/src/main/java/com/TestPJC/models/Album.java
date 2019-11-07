0367aae5c8c077236b0eb50f5c22f39d4c6e220b
edermarcelo10 <edermarcelo10@gmail.com>
2019-11-07 11:59:46 -0300

    Create Album.java
    Reposicionamento de classe para dentro do package models

---
+++ TestPJCAlbum/src/main/java/com/TestPJC/models/Album.java
@@ -0,0 +1,56 @@
+package com.TestPJC.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+@Entity
+public class Album {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long Id_Album;
+	
+	@NotBlank
+	private String Descricao_Album;
+	
+	@NotBlank
+	private long Id_Artista;
+	
+	@NotBlank
+	private String Nome_Artista;
+
+	public String getNome_Artista() {
+		return Nome_Artista;
+	}
+
+	public void setNome_Artista(String nome_Artista) {
+		Nome_Artista = nome_Artista;
+	}
+
+	public long getId_Album() {
+		return Id_Album;
+	}
+
+	public void setId_Album(long id_Album) {
+		Id_Album = id_Album;
+	}
+
+	public String getDescricao_Album() {
+		return Descricao_Album;
+	}
+
+	public void setDescricao_Album(String descricao_Album) {
+		Descricao_Album = descricao_Album;
+	}
+
+	public long getId_Artista() {
+		return Id_Artista;
+	}
+
+	public void setId_Artista(long id_Artista) {
+		Id_Artista = id_Artista;
+	}
+}
