01b0a50b5a13ca6efc0810d86ad5f055464623be
edermarcelo10 <edermarcelo10@gmail.com>
2019-11-07 10:02:20 -0300

    Update service.java
    Inclusão dos métodos relacionados a tabela de albuns para acesso via API.

--- TestPJCAlbum/src/main/java/com/TestPJC/service.java
+++ TestPJCAlbum/src/main/java/com/TestPJC/service.java
@@ -10,10 +10,13 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/service")
 public class service {
-	/*INJEÇÃO DE DEPÊNDENCIA*/
+	
+	/* Metodos CRUD da tabela CAD_ARTISTA
+	 * Autor: Eder Marcelo
+	 * */
 	@Autowired
 	private ArtistaRepository ArtistaRepository = new ArtistaRepository();
- 
+
 	@RequestMapping(value="/InsereArtista/{Nome}")
 	public ResultadoModel InsereArtista (@PathVariable("Nome") String Nome){
  
@@ -42,5 +45,41 @@ public class service {
 	public @ResponseBody Artista SelecionaArtista(@PathVariable("ID") Integer ID){
  
 		return this.ArtistaRepository.SelecionaArtista(ID);
+	}
+	
+	/* Metodos CRUD da tabela CAD_ALBUM
+	 * Autor: Eder Marcelo
+	 * */
+	@Autowired
+	private AlbumRepository AlbumRepository = new AlbumRepository();
+
+	@RequestMapping(value="/InsereAlbum/{Descricao}/{Id_Artista}")
+	public ResultadoModel InsereAlbum (@PathVariable("Descricao") String Descricao, @PathVariable("Id_Artista") int Id_Artista){
+ 
+		return this.AlbumRepository.InsereAlbum(Descricao, Id_Artista);
+	}
+	
+	@RequestMapping(value="/AtualizaAlbum/{ID}/{Descricao}/Id_Artista}")
+	public @ResponseBody ResultadoModel AtualizaAlbum(@PathVariable("ID") Integer ID, @PathVariable("Descricao") String Descricao, @PathVariable("Id_Artista") Integer Id_Artista){
+ 
+		return this.AlbumRepository.AtualizaAlbum(ID, Descricao, Id_Artista);
+	}
+	
+	@RequestMapping(value="/ExcluirAlbum/{ID}")
+	public @ResponseBody ResultadoModel ExcluirAlbum(@PathVariable("ID") Integer ID){
+ 
+		return this.AlbumRepository.ExcluirAlbum(ID);
+	}
+		
+	@RequestMapping(value="/SelecionaListaAlbuns")
+	public List<Album> SelecionaListaAlbuns()
+	{
+		return this.AlbumRepository.SelecionaListaAlbuns(); 
+	}
+	
+	@RequestMapping(value="/SelecionaAlbum/{ID}/{Id_Artista}")
+	public @ResponseBody Album SelecionaAlbum(@PathVariable("ID") Integer ID, @PathVariable("Id_Artista") Integer Id_Artista){
+ 
+		return this.AlbumRepository.SelecionaAlbum(ID, Id_Artista);
 	}
 }
